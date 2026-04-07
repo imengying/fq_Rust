aed64a54ed93a2dad84048848202a63783a60d35
imengying <yuzucv@gmail.com>
2026-04-07 22:37:15 +0800

    Enable HTTP/3 client transport

--- api/src/state.rs
+++ api/src/state.rs
@@ -29,7 +29,7 @@ pub struct AppState {
 impl AppState {
     pub async fn new(config: AppConfig) -> Result<Arc<Self>> {
         let http_client = reqwest::Client::builder()
-            .http1_only()
+            .http3_prior_knowledge()
             .no_gzip()
             .connect_timeout(Duration::from_millis(
                 config.fq.upstream.connect_timeout_ms,
