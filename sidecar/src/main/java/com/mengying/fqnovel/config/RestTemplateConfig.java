63ffa9ce5be0555dbe60b2c7cae1ac97723aa5ef
imengying <yuzucv@gmail.com>
2026-04-07 17:19:01 +0800

    Use plain RestTemplate factory

--- sidecar/src/main/java/com/mengying/fqnovel/config/RestTemplateConfig.java
+++ sidecar/src/main/java/com/mengying/fqnovel/config/RestTemplateConfig.java
@@ -1,20 +1,18 @@
 package com.mengying.fqnovel.config;
 
-import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder, SidecarUpstreamProperties properties) {
-        return builder
-            .setConnectTimeout(Duration.ofMillis(Math.max(1000L, properties.getConnectTimeoutMs())))
-            .setReadTimeout(Duration.ofMillis(Math.max(1000L, properties.getReadTimeoutMs())))
-            .build();
+    public RestTemplate restTemplate(SidecarUpstreamProperties properties) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout((int) Math.max(1000L, properties.getConnectTimeoutMs()));
+        requestFactory.setReadTimeout((int) Math.max(1000L, properties.getReadTimeoutMs()));
+        return new RestTemplate(requestFactory);
     }
 }
